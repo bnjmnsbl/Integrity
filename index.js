6356ccac978e2939e242c5fb898d0fb7325b03a2
Ben <ben3000@gmx.de>
2017-02-12 17:36:54 +0000

    original

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express")
 var app = express();
 var mongo = require("mongodb").MongoClient;
 var check = require("./urlCheck.js");
-
+var mongoUrl = 'mongodb://localhost:27017/benDB'
 
 var db;
 var port = process.env.PORT;
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + "/public"))
 app.set("view engine", "ejs");
 
 
-mongo.connect('mongodb://localhost:27017/benDB', (err, database) => { 
+mongo.connect(mongoUrl, (err, database) => { 
     if(err) return console.log(err);
     db = database;
     app.listen(port, function() {
@@ -19,44 +19,25 @@ mongo.connect('mongodb://localhost:27017/benDB', (err, database) => {
 })
 })
 
-app.get("/new/*", (req, res) => {
-    var urlLong = req.originalUrl.substring(5)
-    var urlShort = "";
-
-    urlLong = check.urlAdapt(urlLong);
-
- // check if url already exists in database
-      db.collection("urls").findOne({long: urlLong}, function (err, doc){
-    if (err) console.log(err);
-    if (doc){
-      console.log("Entry already found");
-      res.render("pages/shortlink", {link: doc.short});
-    } else {
-      // The long URL was not found in the long_url field in our urls
-      // collection, so we need to create a new entry
-
-    if (check.urlCheck(urlLong)) {
-        console.log(urlLong + " returned true");
-        urlShort = check.genShort();
-        db.collection("urls").save({long: urlLong, short: urlShort}, (err, result) => {
-        if (err) console.log(err);     
-        });
-        res.render("pages/shortlink", {link: urlShort})
-    } else {
+app.get("/check/*", (req, res) => {
+    var urlLong = req.originalUrl.substring(7);
+    console.log("Got URL " + urlLong)
         
+    // check if url exists in database
+    db.collection("news").find({long: urlLong}).toArray(function(err,doc) {
+        if (err) console.log(err);
+        if (doc.length > 0){
+            console.log("heyo " + doc[0].integrity);
+            res.render("pages/index", {link: doc});
+    } else {
+
+        console.log("Not found");
         res.render("pages/error")
-    }    }
+    }    
   });
 
 });
 
 app.get("/*", (req, res)=> {
-    var shortLink = req.originalUrl.substring(1);
-     db.collection("urls").find({short: shortLink}).toArray(function(err, results) {
-        if (err) return console.log(err) 
-        
-        res.render("pages/index", {link: results})
-       
-    })
-    
+    res.send("Home Page");
 });
--- readme.md
+++ readme.md
@@ -1,18 +1,7 @@
-# URL MINIONATOR
+# Integrity Checker
 
-## A microservice that creates long, complex and minionated URLs
+## How credible is this source?
 
-It works like an URL shortener, but translates them into minion language (which makes most URLs actually longer, duh!). 
-
-This probably makes not much sense, but there you go. 
-
-You CAN turn this app into a URL shortener easily, by making a few changes to the genShort() function. You'll probably figure that out. 
-
-## Usage
-
-Create a new minionated URL by calling localhost/new/[www.YOURURL.com]
-
-
-Enjoy!
+New project im working on, nothing to see yet. 
 
 Build with Node.js, Express, MongoDB and EJS
