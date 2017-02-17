fe4888fa3f05d60c9a4e7da423822ee9b0b09860
Ben <ben3000@gmx.de>
2017-02-17 10:52:04 +0000

    de-minionized

--- urlCheck.js
+++ urlCheck.js
@@ -22,15 +22,4 @@ return /^(?:(?:(https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3
 };
 
 
-exports.genShort = function(value) {
-   
-    var text = "";
-    var minion = ["Minions", "ipsum", "poulet", "tikka", "masala", "tatata", "bala", "tu", "potatoooo", "aliqua", "cillum", "sed", "reprehenderit", "poulet", "tikka", "masala", "elit", "consectetur", "Potatoooo", "potatoooo", "chasy", "exercitation", "hana", "dul", "sae", "Ti", "aamoo", "voluptate", "laboris", "irure", "pepete", "magna", "quis", "consectetur", "reprehenderit", "Tank", "yuuu", "la", "bodaaa", "incididunt", "duis", "velit", "Laboris", "officia", "commodo", "ut", "nostrud", "enim", "Sed", "irure", "bee", "do", "bee", "do", "bee", "do", "aliquip", "underweaaar", "eiusmod", "Dolor", "voluptate", "ti", "aamoo", "Poulet", "tikka", "masala", "tatata", "bala", "tu", "adipisicing", "Underweaaar", "ex", "aliquip", "esse", "minim", "gelatooo", "wiiiii", "hana", "dul", "sae", "consequat", "Bananaaaa", "qui", "et", "dolor", "aute", "minim", "Tempor", "incididunt", "elit", "tank", "yuuu", "Aute", "uuuhhh", "aliquip", "enim", "laboris", "esse", "Consequat", "baboiii", "labore", "butt", "adipisicing", "pepete", "reprehenderit", "jiji", "me", "want", "bananaaa", "Bee", "do", "bee", "do", "bee", "do", "labore", "tatata", "bala", "tu", "elit", "Ad", "jeje", "irure", "elit", "eiusmod", "tank", "yuuu!", "Hana", "dul", "sae"]
-    //var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for( var i=0; i < 10; i++ )
-        text += minion[(Math.floor(Math.random() * minion.length))];
-
-    return text;
-}
 
